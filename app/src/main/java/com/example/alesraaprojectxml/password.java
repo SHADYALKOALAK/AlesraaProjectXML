b9af32d274568fb520e4ce0838cd8d5124689a2f
baraakmail2004 <aabukmail3@smail.ucas.edu.ps>
2023-07-08 20:22:02 +0300

    update from this project

--- app/src/main/java/com/example/alesraaprojectxml/password.java
+++ app/src/main/java/com/example/alesraaprojectxml/password.java
@@ -10,5 +10,7 @@ public class password extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_password);
+
+
     }
 }
