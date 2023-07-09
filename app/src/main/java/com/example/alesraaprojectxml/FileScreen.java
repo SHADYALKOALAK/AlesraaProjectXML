9672b0108b65e9d198a93ec577afba29fdfdd1f4
living_space <sa187@smail.ucas.edu.ps>
2023-07-09 15:25:24 +0300

    rc

--- app/src/main/java/com/example/alesraaprojectxml/FileScreen.java
+++ app/src/main/java/com/example/alesraaprojectxml/FileScreen.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 
 import com.example.alesraaprojectxml.databinding.ActivityFileScreenBinding;
@@ -28,10 +30,15 @@ public class FileScreen extends AppCompatActivity {
         commentsModels = new ArrayList<>();
         dataBase = new DBase(context);
         rc_comment = new Rc_Comment(context, commentsModels);
+        Cursor cursor = dataBase.getComment();
+        while (cursor.moveToNext()) {
+            @SuppressLint("Range") String comment = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(1)));
+            commentsModels.add(new CommentsModel(comment));
+        }
+//        commentsModels.add(new CommentsModel(" هدية خليل مقاط /", "يعطيك ألف عافية دكتور"));
+//        commentsModels.add(new CommentsModel("دانية محمود نصر/", "يعطيك ألف عافية دكتور , تم تسليم الواجب"));
+//        commentsModels.add(new CommentsModel("دانية محمود نصر/", "يعطيك ألف عافية دكتور , تم تسليم الواجب"));
 
-        commentsModels.add(new CommentsModel(" هدية خليل مقاط /", "يعطيك ألف عافية دكتور"));
-        commentsModels.add(new CommentsModel("دانية محمود نصر/", "يعطيك ألف عافية دكتور , تم تسليم الواجب"));
-        commentsModels.add(new CommentsModel("دانية محمود نصر/", "يعطيك ألف عافية دكتور , تم تسليم الواجب"));
         binding.rcComments.setAdapter(rc_comment);
         binding.rcComments.setLayoutManager(new LinearLayoutManager(context, RecyclerView.VERTICAL, false));
         binding.btnSend.setOnClickListener(v -> {
