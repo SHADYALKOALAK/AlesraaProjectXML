687f4883098ef4c29f60fa5ae876e8c2826b0595
baraakmail2004 <aabukmail3@smail.ucas.edu.ps>
2023-07-09 10:41:13 +0300

    new update baraa

---
+++ app/src/main/java/com/example/alesraaprojectxml/CommentsAdmin.java
@@ -0,0 +1,38 @@
+package com.example.alesraaprojectxml;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import com.example.alesraaprojectxml.databinding.ActivityCommentsAdminBinding;
+
+import java.util.ArrayList;
+
+public class CommentsAdmin extends AppCompatActivity {
+    Context context=CommentsAdmin.this;
+    ArrayList<ItemCommentsAdmin> arrayList;
+    AdapterCommentsAdmin adapterCommentsAdmin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCommentsAdminBinding binding;
+        binding=ActivityCommentsAdminBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        arrayList=new ArrayList<>();
+        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
+        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
+        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
+        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
+        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
+        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
+        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
+        adapterCommentsAdmin=new AdapterCommentsAdmin(context,arrayList);
+        binding.rv.setAdapter(adapterCommentsAdmin);
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(context);
+        binding.rv.setLayoutManager(linearLayoutManager);
+
+    }
+}
