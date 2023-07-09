119a385e184562d983371b548e392f066a25e985
baraakmail2004 <aabukmail3@smail.ucas.edu.ps>
2023-07-09 20:51:39 +0300

    new update baraa shady

--- app/src/main/java/com/example/alesraaprojectxml/MainWelcomeScreens.java
+++ app/src/main/java/com/example/alesraaprojectxml/MainWelcomeScreens.java
@@ -25,7 +25,7 @@ public class MainWelcomeScreens extends AppCompatActivity {
         skip = findViewById(R.id.tv_skip);
 
         skip.setOnClickListener(v -> {
-            Intent intent = new Intent(context, SignUpScreen.class);
+            Intent intent = new Intent(context, LoginScreen.class);
             startActivity(intent);
             finish();
         });
