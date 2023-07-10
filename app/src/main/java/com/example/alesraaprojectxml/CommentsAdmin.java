74d560c37b508b3813f38df55c56d91982b77fcd
living_space <sa187@smail.ucas.edu.ps>
2023-07-10 17:27:40 +0300

    rebly

--- app/src/main/java/com/example/alesraaprojectxml/CommentsAdmin.java
+++ app/src/main/java/com/example/alesraaprojectxml/CommentsAdmin.java
@@ -3,8 +3,10 @@ package com.example.alesraaprojectxml;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 
@@ -13,28 +15,36 @@ import com.example.alesraaprojectxml.databinding.ActivityCommentsAdminBinding;
 import java.util.ArrayList;
 
 public class CommentsAdmin extends AppCompatActivity {
-    Context context=CommentsAdmin.this;
-    ArrayList<ItemCommentsAdmin> arrayList;
-    AdapterCommentsAdmin adapterCommentsAdmin;
+    private Context context = CommentsAdmin.this;
+    private ArrayList<ItemCommentsAdmin> arrayList;
+    private AdapterCommentsAdmin adapterCommentsAdmin;
+    private DBase dBase;
+    private String nameProfile;
 
+    @SuppressLint("Range")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityCommentsAdminBinding binding;
-        binding=ActivityCommentsAdminBinding.inflate(getLayoutInflater());
+        binding = ActivityCommentsAdminBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        arrayList=new ArrayList<>();
-        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
-        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
-        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
-        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
-        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
-        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
-        arrayList.add(new ItemCommentsAdmin("Baraa","this is my comments"));
-        adapterCommentsAdmin=new AdapterCommentsAdmin(context,arrayList);
+        arrayList = new ArrayList<>();
+        dBase = new DBase(context);
+
+        adapterCommentsAdmin = new AdapterCommentsAdmin(context, arrayList);
         binding.rv.setAdapter(adapterCommentsAdmin);
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(context);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(context);
         binding.rv.setLayoutManager(linearLayoutManager);
+        Cursor cursor = dBase.getComment();
+        while (cursor.moveToNext()) {
+            nameProfile = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(2)));
+        }
+
+        Cursor commentAdmin = dBase.getCommentAdmin();
+        while (commentAdmin.moveToNext()) {
+            @SuppressLint("Range") String comment = commentAdmin.getString(commentAdmin.getColumnIndex(commentAdmin.getColumnName(1)));
+            arrayList.add(new ItemCommentsAdmin(nameProfile, comment));
+        }
 
 
         binding.iconHomework.setOnClickListener(v -> {
@@ -46,7 +56,7 @@ public class CommentsAdmin extends AppCompatActivity {
         binding.imageProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(context,Admin_1.class));
+                startActivity(new Intent(context, Admin_1.class));
             }
         });
 
